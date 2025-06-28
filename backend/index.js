589a839e239f3b2387c771ef2f85c3dfc8010b96
dratum <devoonwtf@gmail.com>
2025-06-28 20:59:36 +0300

    Добавление prettier

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express()
-const PORT = 3000 || process.env.PORT
+const PORT = 3001 || process.env.PORT
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
